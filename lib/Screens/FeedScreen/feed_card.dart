8ceff3ee44958ea6eac13cbebb0e1a97d04ae1fe
FirefoxSRV <shreyasvisweshwaran@gmail.com>
2023-11-12 19:02:40 +0530

    club posters

--- lib/Screens/FeedScreen/feed_card.dart
+++ lib/Screens/FeedScreen/feed_card.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -14,9 +11,15 @@ class FeedCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var containerHeight = MediaQuery.of(context).size.height;
+    var containerWidth = MediaQuery.of(context).size.width;
     return GestureDetector(
-      onTap: (){
-
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return OpenMaterialAlertDialog(imageUrl: 'assets/images/club_poster_1.jpg',containerWidth: containerWidth, containerHeight: containerHeight, title: "DJ Night OLAM'22", content: "Acharya Hall, AB1");
+            });
       },
       child: Container(
           decoration: BoxDecoration(color: Colors.white, border: Border.all(color: Colors.blueGrey), borderRadius: BorderRadius.circular(30)),
@@ -39,15 +42,15 @@ class FeedCard extends StatelessWidget {
               Row(
                 children: [
                   Padding(
-                    padding: EdgeInsets.only(left: width*0.03),
+                    padding: EdgeInsets.only(left: width * 0.03),
                     child: Center(
                         child: Text(
-                          "Workshop",
-                          style: GoogleFonts.quicksand(fontSize: 20, fontWeight: FontWeight.w900),
-                        )),
+                      "Workshop",
+                      style: GoogleFonts.quicksand(fontSize: 20, fontWeight: FontWeight.w900),
+                    )),
                   ),
                   VerticalDivider(
-                    width: width*0.1,
+                    width: width * 0.1,
                     thickness: 1,
                     color: Colors.black,
                     endIndent: height * 0.03,
@@ -59,8 +62,8 @@ class FeedCard extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         SizedBox(height: height * 0.01),
-                        Text("Venue: C102 5:30pm",style: GoogleFonts.quicksand(fontSize: 20, fontWeight: FontWeight.bold)),
-                        Text("Click to register",style: GoogleFonts.quicksand(fontSize: 15, fontWeight: FontWeight.bold)),
+                        Text("Venue: C102 5:30pm", style: GoogleFonts.quicksand(fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text("Click to register", style: GoogleFonts.quicksand(fontSize: 15, fontWeight: FontWeight.bold)),
                         SizedBox(height: height * 0.01),
                       ],
                     ),
@@ -69,6 +72,70 @@ class FeedCard extends StatelessWidget {
               )
             ],
           )),
+    );
+  }
+}
+
+
+
+class OpenMaterialAlertDialog extends StatelessWidget {
+  const OpenMaterialAlertDialog({super.key, required this.imageUrl,required this.containerHeight, required this.containerWidth, required this.title, required this.content});
+  final double containerWidth;
+  final double containerHeight;
+  final String title;
+  final String content;
+  final String imageUrl;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Theme.of(context).dialogBackgroundColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
+      titlePadding: EdgeInsets.all(0),
+      title: Image.asset(
+        imageUrl,
+        fit: BoxFit.fill,
+        height: containerHeight*0.6,
+        width: containerWidth,
+      ),
+      content: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: containerWidth*0.8,
+          height: containerHeight * 0.1,
+          child: Row(
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          title,
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.quicksand(fontSize: 25,fontWeight: FontWeight.w800),
+                        ),
+                        IconButton(icon:Icon(Icons.keyboard_double_arrow_right,color: Colors.black,size: 50,), onPressed: () {  },)
+                      ],
+                    ),
+                    Divider(thickness: 3,color: Colors.black,indent: containerWidth*0.1,endIndent: containerWidth*0.1,),
+                    Text(
+                      content,
+                      style: GoogleFonts.quicksand(fontSize: 18, fontWeight: FontWeight.normal, color: Theme.of(context).disabledColor),
+                    ),
+                    SizedBox(height: containerHeight*0.01,),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
